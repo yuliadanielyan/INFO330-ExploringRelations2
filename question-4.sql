45ae6eb878ac3bed8790e9d03c8766448f20b521
yuliadanielyan <114948223+yuliadanielyan@users.noreply.github.com>
2023-04-11 20:16:57 -0700

    Update question-4.sql

--- question-4.sql
+++ question-4.sql
@@ -1,1 +1,7 @@
 -- Which sales agent made the most in sales in 2010?
+
+SELECT e.FirstName || ' ' || e.LastName AS sales_agent_name, SUM(ii.UnitPrice * ii.Quantity) AS sales FROM customers c
+ JOIN invoices i ON c.CustomerId = i.CustomerId
+ JOIN invoice_items ii ON i.InvoiceId = ii.InvoiceId
+ JOIN employees e ON c.SupportRepId = e.EmployeeId WHERE i.InvoiceDate LIKE '2010-%'
+GROUP BY e.EmployeeId ORDER BY sales DESC LIMIT 1;
